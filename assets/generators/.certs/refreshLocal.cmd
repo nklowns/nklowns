1e4a7b01750a4344e90fc5e2300b143b81cb5cf5
Noocktall Klown S <nklowns@gmail.com>
2022-12-11 07:10:56 -0300

    feat: fake generators certs

---
+++ assets/generators/.certs/refreshLocal.cmd
@@ -0,0 +1,2 @@
+openssl req -new -nodes -newkey rsa:2048 -keyout localhost.key -out localhost.csr -subj "/C=<PAIS>/ST=<ESTADO>/L=<CIDADE>/O=<ORG>-Certificates/CN=localhost"
+openssl x509 -req -sha256 -days 1024 -in localhost.csr -CA RootCA.pem -CAkey RootCA.key -CAcreateserial -extfile domains.ext -out localhost.crt
